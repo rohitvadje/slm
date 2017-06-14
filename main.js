ed13a911675886fde4b43da7039bad76380e73cc
rohitvadje <rohit.8859@gmail.com>
2017-06-14 20:39:17 +0530

    Subscribing to push

--- main.js
+++ main.js
@@ -16,17 +16,45 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 }
 
 function initialiseUI() {
+	pushButton.addEventListener('click', function() {
+		pushButton.disabled = true;
+		if (isSubscribed) {
+			// TODO: Unsubscribe user
+		} else {
+			subscribeUser();
+		}
+	});
+	
 	// Set the initial subscription value
 	swRegistration.pushManager.getSubscription()
 	.then(function(subscription) {
 		isSubscribed = !(subscription === null);
-
 		if (isSubscribed) {
 			console.log('User IS subscribed.');
 		} else {
 			console.log('User is NOT subscribed.');
 		}
+		updateBtn();
+	});
+}
 
+function subscribeUser() {
+	const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+	swRegistration.pushManager.subscribe({
+		userVisibleOnly: true,
+		applicationServerKey: applicationServerKey
+	})
+	.then(function(subscription) {
+		console.log('User is subscribed.');
+
+		updateSubscriptionOnServer(subscription);
+
+		isSubscribed = true;
+
+		updateBtn();
+	})
+	.catch(function(err) {
+		console.log('Failed to subscribe the user: ', err);
 		updateBtn();
 	});
 }
@@ -40,3 +68,11 @@ function updateBtn() {
 
 	pushButton.disabled = false;
 }
+
+navigator.serviceWorker.register('sw.js')
+.then(function(swReg) {
+	console.log('Service Worker is registered', swReg);
+
+	swRegistration = swReg;
+	initialiseUI();
+})
