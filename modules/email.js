6a8b03efe08422e64868808a9f59274ecd27ff85
rohitvadje <rvadje@cisco.com>
2017-06-30 16:20:39 +0530

    1. Email modules created 2. Database connection module created

---
+++ modules/email.js
@@ -0,0 +1,8 @@
+/*
+ * Author : rohit.8859@gmail.com	
+ * Module : Send email notification to registered users  
+ */
+
+exports.sendMail = function(senderId, senderPwd, subject, body){
+	
+}
---
+++ modules/mongo_connection.js
@@ -0,0 +1,6 @@
+/*
+ * Author : rohit.8859@gmail.com
+ * Module : Connect / disconnect to database 
+ */
+
+
