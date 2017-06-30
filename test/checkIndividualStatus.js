d6f407fe9d36e7b122a697d62c7d0c0301c8f5a4
rohitvadje <rvadje@cisco.com>
2017-06-30 11:54:31 +0530

    Test for live check functions

---
+++ test/checkIndividualStatus.js
@@ -0,0 +1,10 @@
+/*
+ * Author : rohit.8859@gmail.com
+ * Test module : checking individual status of avialable hosts
+ */
+
+var live = require('../modules/live_check')
+live.checkStatus('Dev');
+live.checkStatus('Dev1');
+live.checkStatus('Stage');
+live.checkStatus('Bitbucket');
